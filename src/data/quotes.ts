45be90799ebbe9f8ebc969bc0a8e2f47f1f5b25c
erasmus <ras.hagen@gmail.com>
2026-08-18 11:14:39 +0200

    Update quotes

--- src/data/quotes.ts
+++ src/data/quotes.ts
@@ -4,10 +4,10 @@ export interface Quote { name: string; quote: string; image?: string }
 
 export const QUOTES: Quote[] = [
   // Arjun Yadav
-  { name: 'Arjun', image: 'arjun.jpeg', quote: 'Erasmus leads with low ego and a strong focus on outcomes, while always showing patience and empathy toward those around him.' },
-  { name: 'Torsten', image: 'torsten.jpeg', quote: 'A consistent beacon of reliability, with an unwavering appetite for novel strategies, Erasmus is an invaluable asset to any team.' },
-  { name: 'Sebastian', image: 'sebastian.jpeg', quote: 'Erasmus was without a doubt one the best leaders I worked with.' },
+  { name: 'Arjun Yadav', image: 'arjun.jpeg', quote: 'Erasmus leads with low ego and a strong focus on outcomes.' },
+  { name: 'Dr. Torsten Stüber', image: 'torsten.jpeg', quote: 'A consistent beacon of reliability, with an unwavering appetite for novel strategies.' },
+  { name: 'Sebastian Nause-Blüml', image: 'sebastian.jpeg', quote: 'One of the best leaders I worked with.' },
   // Dario Perić
-  { name: 'Dario', image: 'dario.jpeg', quote: 'Erasmus has a rare way of carrying real, tangible authority that comes through naturally in his actions.' },
-  { name: 'Prayag', image: 'prayag.jpeg', quote: 'His clarity of thought in every situation proves invaluable for any team, coupled with his ability to align people towards an objective and deliver great results.' },
+  { name: 'Dario Perić', image: 'dario.jpeg', quote: 'Erasmus has a rare way of carrying real, tangible authority that comes through naturally in his actions.' },
+  { name: 'Prayag Desale', image: 'prayag.jpeg', quote: 'His clarity of thought in every situation proves invaluable.' },
 ];
